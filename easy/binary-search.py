6584090b7e4ced13f57dc029c09b46cba6440a06
Chris Muga <ChrisMuga94@Gmail.com>
2025-03-13 20:16:15 +0300

    binary-search: init

---
+++ easy/binary-search.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        x = 0
+        y = len(nums) - 1
+
+        while x <= y:
+            if nums[x] == target:
+                return x
+            elif nums[y] == target:
+                return y
+            x += 1
+            y -= 1
+        
+        return -1
+
+solution = Solution()
+ans = solution.search([5], 5)
+print(ans)
+
