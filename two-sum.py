f80e3933ebe5da97e085e0980a722e84922766c4
Chris Muga <ChrisMuga94@Gmail.com>
2025-03-13 14:06:05 +0300

    two-sum

---
+++ two-sum.py
@@ -0,0 +1,21 @@
+# Category: Easy
+# https://leetcode.com/problems/two-sum/description/
+from typing import List
+
+class Solution:
+    def twoSum(self, nums, target):
+        pairs = {}
+
+        # Load pairs
+        for (idx, num) in enumerate(nums):
+            diff = target - num
+            match = pairs.get(num)
+            if match != None:
+                return [match, idx]
+            else:
+                pairs[diff] = idx
+
+
+solution = Solution()
+ans = solution.twoSum([2,7,11,15], 9)
+print(ans)
