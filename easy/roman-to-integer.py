cc416f458ecdcf24ec59d8f6779a88b1cadd8213
Chris Muga <chrismuga94@gmail.com>
2025-05-13 11:50:04 +0300

    comment out brute force approach

--- easy/roman-to-integer.py
+++ easy/roman-to-integer.py
@@ -2,4 +2,21 @@
 # https://leetcode.com/problems/roman-to-integer/description/
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        symbols = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000,
+        }
+        total = 0
+        for i in s:
+            print(i)
+            total += symbols[i]
+        print("total: ", total)
+
+solution = Solution()
+sample = "MCMXCIV"
+ans = solution.romanToInt(sample)
--- medium/longest-substring-without-repeating-chars.py
+++ medium/longest-substring-without-repeating-chars.py
@@ -4,33 +4,34 @@
 # Given a string, s, find the length of the longest substring without duplicate characters.
 # TODO: Implement O(n) solution with sliding window
 
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        i = 0
-        j = 0
-        buff = []
-        current_max = 0
-
-        while(i <= len(s) - 1):
-            curr = ""
-
-            while(s[j] not in curr):
-                print(s, j)
-                curr += s[j]
-
-                if(j < len(s) - 1):
-                    j += 1
-
-            # print("***", len(curr))
-            current_max = max(current_max, len(curr))
-            buff.append(curr)
-            # print(buff)
-
-            i += 1
-            j = i
-
-
-        return current_max
+# Brute force approach O(n ^ 2)
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         i = 0
+#         j = 0
+#         buff = []
+#         current_max = 0
+#
+#         while(i <= len(s) - 1):
+#             curr = ""
+#
+#             while(s[j] not in curr):
+#                 print(s, j)
+#                 curr += s[j]
+#
+#                 if(j < len(s) - 1):
+#                     j += 1
+#
+#             # print("***", len(curr))
+#             current_max = max(current_max, len(curr))
+#             buff.append(curr)
+#             # print(buff)
+#
+#             i += 1
+#             j = i
+#
+#
+#         return current_max
 
 solution = Solution()
 ans = solution.lengthOfLongestSubstring("c")
