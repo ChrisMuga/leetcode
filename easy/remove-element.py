af35bc43d934b5950f149fbb6f4f16670b14b0a4
Chris Muga <chrismuga94@gmail.com>
2025-05-13 14:51:05 +0300

    init: Remove element

---
+++ easy/remove-element.py
@@ -0,0 +1,12 @@
+# difficulty: easy
+# https://leetcode.com/problems/remove-element/description/
+
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        pass
+
+# Run
+solution = Solution()
+ans = solution.removeElement([3,2,2,3], 3)
+print(ans)
