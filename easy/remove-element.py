fb9cff7c219e1125d32a32b5b0aead91dc3f6a51
Chris Muga <chrismuga94@gmail.com>
2025-05-13 15:47:37 +0300

    remove item logic

--- easy/remove-element.py
+++ easy/remove-element.py
@@ -1,12 +1,24 @@
 # difficulty: easy
 # https://leetcode.com/problems/remove-element/description/
 
+"""
+    two pointers - i, j
+    while i < j:
+
+"""
 from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        pass
+        idx = 0
+        for num in nums:
+            if num != val:
+                nums[idx] = num
+                idx += 1
+
+        return idx 
+
 
 # Run
 solution = Solution()
-ans = solution.removeElement([3,2,2,3], 3)
+ans = solution.removeElement([0,1,2,2,3,0,4,2], 2)
 print(ans)
