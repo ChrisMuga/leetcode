0cfe994914e1d417c187ca6cd4f6f909c140f3a4
Chris Muga <ChrisMuga94@Gmail.com>
2025-05-17 17:12:50 +0300

    index of first occurance in a string
    Python Solution

---
+++ easy/index-of-first-occurance.py
@@ -0,0 +1,25 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        starter = needle[0]
+
+        if(len(haystack) < len(needle)):
+            return -1
+        
+        for idxI, i in enumerate(haystack):
+            if i == starter:
+                running_slice = ""
+                for idxJ, j in enumerate(needle):
+                    pointer = idxI + idxJ
+                    if pointer >= len(haystack):
+                        return -1
+                    if haystack[pointer] == j:
+                        running_slice += j
+                if running_slice == needle:
+                    return idxI
+        return -1
+
+
+# Run
+solution = Solution()
+res = solution.strStr("sadbutsad", "sad")
+print(res)
