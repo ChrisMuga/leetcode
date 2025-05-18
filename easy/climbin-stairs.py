6cd0de099826ee39d2eaab762e2fdc55734ea1b6
Chris Muga <ChrisMuga94@Gmail.com>
2025-05-18 09:55:38 +0300

    Linear backtracking - O(N)

--- easy/climbin-stairs.py
+++ easy/climbin-stairs.py
@@ -4,13 +4,29 @@
 """
 
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n == 0:
-            return 1
-        if n == 1:
-            return 1
+    # # Using recursion - recursive backtracking
+    # # O(2 ^ N)
+    # def climbStairs(self, n: int) -> int:
+    #     if n == 0:
+    #         return 1
+    #     if n == 1:
+    #         return 1
+    #
+    #     return self.climbStairs(n -1) + self.climbStairs(n - 2)
 
-        return self.climbStairs(n -1) + self.climbStairs(n - 2)
+    # O(N) - Backtracking
+    def climbStairs(self, n: int) -> int:
+        if n == 1 or n == 0:
+            return 1
+        
+        # initialize with (i-2) and (i-1)
+        buff = [1, 1]
+
+        for i in range(2, n + 1):
+            buff.append(buff[i-1] + buff[i-2])
+
+        print(buff[n])
+
 
 
 solution = Solution()
