ee3dbf36f4904f485bffd63e65651aa485d98260
Chris Muga <ChrisMuga94@Gmail.com>
2025-05-21 02:15:41 +0300

    Init with dummy code

---
+++ easy/merge-two-sorted-lists.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Definition for singly-linked list.
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+ struct ListNode* res = { 4, NULL }; 
+
+ return res;
+}
+
+int main(){
+	/* struct ListNode* c = {4, NULL}; */
+	/* struct ListNode* b = {3, c}; */
+	/* struct ListNode* a =  {1, b}; */
+
+	struct ListNode* test;
+	test->val = 10;
+	test->next = NULL;
+
+	if(test){
+		printf("%d", test->val);
+	}
+	return 0;
+}
