2957429918201bc03cde4217486fd0825073cea6
Chris Muga <ChrisMuga94@Gmail.com>
2025-11-13 15:44:03 +0300

    alt solution: stock

--- easy/stock.py
+++ easy/stock.py
@@ -1,6 +1,22 @@
 from typing import List
 
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=problem-list-v2&envId=array
+
+# Alt solution
+# def maxProfit(self, prices: List[int]) -> int:
+# 	i, j, prof_max = 0, 1, 0
+# 
+# 	while(j < len(prices)):
+# 	    price_i = prices[i]
+# 	    price_j = prices[j]
+# 
+# 	    if price_i > price_j:
+# 		i = j
+# 	    else:
+# 		prof_max = max(prof_max, price_j - price_i)
+# 		j += 1
+
+return prof_max
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_price = prices[0]
