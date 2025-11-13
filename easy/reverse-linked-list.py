821139eb739b5610c00af977d5a02af60231c555
Chris Muga <ChrisMuga94@Gmail.com>
2025-11-13 15:15:08 +0300

    stock

--- easy/reverse-linked-list.py
+++ easy/reverse-linked-list.py
@@ -1,8 +1,15 @@
+from typing import List, Optional
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        pass
+
+
+sample = createListNode([1, 2, 3, 4, 5])
