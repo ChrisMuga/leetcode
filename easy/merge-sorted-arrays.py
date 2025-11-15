d88dc4f3270d06b808bd86121cfd470ba9963561
Chris Muga <ChrisMuga94@Gmail.com>
2025-11-15 23:01:21 +0300

    merge 2 sorted arrays:  boilerplate

---
+++ easy/merge-sorted-arrays.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+	def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> List[int]:
+		# TODO: Implement
+		return nums1
+
+solution = Solution()
+res = solution.merge([1,2,3,0,0,0], 3, [2,5,6], 3)
+print(res)
+assert res == [1,2,2,3,5,6]
+
+
