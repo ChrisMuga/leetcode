43ae17d6641cce477cae29f49010cb8d7fe5193d
Chris Muga <chrismuga94@gmail.com>
2025-11-16 10:55:19 +0300

    merging 2 sorted arrays: highlight naive approach

--- easy/merge-sorted-arrays-naive.py
+++ easy/merge-sorted-arrays-naive.py
@@ -1,12 +1,24 @@
 from typing import List
 
+"""
+    This is a very naive approach: 
+    - Where we merge the 2 arrays and then perform a sort operation on the result
+"""
 class Solution:
 	def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> List[int]:
-		# TODO: Implement
+		x = m
+		y = 0
+		while x < len(nums1):
+			nums1[x], nums2[y] = nums2[y], nums1[x]
+			x += 1
+			y -= 1
+		nums1.sort()
+
 		return nums1
 
 solution = Solution()
 res = solution.merge([1,2,3,0,0,0], 3, [2,5,6], 3)
+
 print(res)
 assert res == [1,2,2,3,5,6]
 
